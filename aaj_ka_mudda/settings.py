c04e16f381ef487f2c883e88e68b4ed894b684b7
sanjay1994singh <sanjaypython22@gmail.com>
2026-05-09 15:41:15 +0530

    p

--- aaj_ka_mudda/settings.py
+++ aaj_ka_mudda/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'aaj_ka_mudda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aaj_ka_mudda',
+        'USER': 'sanjay',
+        'PASSWORD': 'Hello123456#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
